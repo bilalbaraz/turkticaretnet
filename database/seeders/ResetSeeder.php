619e48028493625272dd3e0e0cc3dc168ead01b5
Bilal Baraz <bilalbaraz@windowslive.com>
2024-11-17 19:49:05 +0300

    Add ResetSeeder to truncate all tables

---
+++ database/seeders/ResetSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cart;
+use App\Models\CartItem;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ResetSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::query()->truncate();
+        Product::query()->truncate();
+        Cart::query()->truncate();
+        CartItem::query()->truncate();
+        Order::query()->truncate();
+    }
+}
